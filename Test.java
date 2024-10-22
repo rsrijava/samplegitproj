e5a5065507b8800de811d7f9783d4bd83bedbdc6
rsrijava <srikarba@gmail.com>
2024-10-21 20:20:53 -0500

    updated Test.java by dev2 for demo for pull

--- Test.java
+++ Test.java
@@ -2,4 +2,8 @@ class Test{
     public void m1(){
         //this is demo for push after clone by dev1
     }
+
+    public static void main(String[] args) {
+        //adding main method as a demo for pull
+    }
 }
